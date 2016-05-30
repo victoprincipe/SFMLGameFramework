a41419b6dd825334e0612133f830d377b3bb35dd
janioxavier <cloud_8945@hotmail.com>
2016-05-30 11:41:57 -0300

    "Classe utilitária para geração de numero aleatorios"

---
+++ SFML-2.3.2/RandomGenerator.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <cstdlib>
+#include <ctime>
+
+class RandomGenerator
+{
+public:
+	float rand_f(int n);
+	int rand_i(int n);
+	RandomGenerator();
+	~RandomGenerator();	
+private:	
+};
